1befff776a00d876af71c6db40bc5807f73cd3a5
Embbnux Ji <embbnux.ji@ringcentral.com>
2026-08-19 14:08:05 +0800

    feat: use ai assistant from cdn

--- config/rc/index.ts
+++ config/rc/index.ts
@@ -25,7 +25,7 @@ export default {
     // platform, so the interop code has to be minted for it.
     clientId:
       process.env.AGENT_ASSISTANT_CLIENT_ID || '',
-    pageUrl: process.env.AGENT_ASSISTANT_PAGE_URL || './agentAssistant.html',
+    pageUrl: process.env.AGENT_ASSISTANT_PAGE_URL || 'https://cdn.labs.ringcentral.com/ringcx-embeddable/ai-assistant/0.0.1/index.html',
   },
   analyticsKey: process.env.MIXPANEL_KEY || '',
   analyticsSecretKey: process.env.ANALYTICS_SECRET_KEY || '',
